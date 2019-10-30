3907b8b1379a58822efd29ffbf7b80e2541e4734
Csatlós Endre <danton392@gmail.com>
2019-10-30 18:38:37 +0100

    HelloWorld létrehozása

---
+++ 20191030HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írd be a neved: "); //Bekéri a nevet sortörés nélkül
+            string nev = Console.ReadLine(); //Változó a nev ebbe tároljuk a nevet
+            Console.Write("Csaaaaaaaaa "); 
+            Console.WriteLine(nev); //kiírja a nevet
+        }
+    }
+}
