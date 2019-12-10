41e17ff274be55b78a1c4c961c16fcd491bc5e9e
Danton <danton392@gmail.com>
2019-12-10 19:52:57 +0100

    HW

---
+++ Homework/HWMűveleti jel/HW műveleti jel/HW műveleti jel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW_műveleti_jel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írd be az első számot!");
+            int bekér1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Írd be a második számot!");
+            int bekér2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Írd be a műveleti jelet!");
+            string művelet = Console.ReadLine();
+            double eredmény = 0;
+            switch (művelet)
+            {
+                case "+": eredmény = bekér1 + bekér2; break;
+                case "-": eredmény = bekér1 - bekér2; break;
+                case "*": eredmény = bekér1 * bekér2; break;
+                case "/": eredmény = bekér1 / bekér2; break;
+            }
+            Console.WriteLine("Eredmény: " + eredmény);
+
+        }
+            
+    }
+}
---
+++ Homework/HWPrímszám/HWPrímszám/HWPrímszám/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HWszökőév
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg az évet : ");
+            int évbekér = int.Parse(Console.ReadLine());
+            if (((évbekér % 4 == 0) && (évbekér % 100 != 0)) || (évbekér % 400 == 0)) Console.WriteLine("{0} ez egy szökőév", évbekér);
+            else Console.WriteLine("{0} Nem szökőév", évbekér);
+            Console.ReadLine();
+        
+        }
+    }
+}
