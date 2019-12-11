4208ce1599b96f83df8a886a6f33fea32de98d78
Csatlós Endre <danton392@gmail.com>
2019-12-11 18:25:42 +0100

    Random szám

---
+++ 20191211Lesson5 Random szám/Lesson5 Random szám/Lesson5 Random szám/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5_Random_szám
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 20191211Lesson5 Random szám/Lesson5RandomSzám/Lesson5RandomSzám/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson5RandomSzám
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bekér;
+            int számláló = 1;
+            int gondoltszám = 10;
+            do
+            {
+                számláló++;
+                Console.Write("Találd el a gondoltszámot ami 1 és 10 között lehet! Írd be milyen számla gondoltál:");
+                bekér = int.Parse(Console.ReadLine());
+                if (bekér < gondoltszám)
+                {
+
+                    Console.WriteLine("A gondoltszám nagyobb mint amire tippeltél! Próbáld újra!");
+
+                }
+                else if (bekér > gondoltszám)
+                {
+
+                    Console.WriteLine("A gondoltszám kisebb mint amire tippeltél! Próbáld újra!");
+
+                }
+            }
+            while (gondoltszám != bekér);
+            Console.WriteLine("Szép volt eltaláltad csapd magad falhoz");
+            Console.Write("Próbálkozások száma: " + számláló);
+
+
+
+        }
+    }
+}
