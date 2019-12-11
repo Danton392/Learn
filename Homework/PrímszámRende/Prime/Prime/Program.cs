e17138dccf1862b28e55e09e05093b87d1cd74ce
Csatlós Endre <danton392@gmail.com>
2019-12-11 18:50:09 +0100

    Prím szám tippelős

---
+++ Homework/PrímszámRende/Prime/Prime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írj be egy számot, kiderül hogy prím e vagy sem!");
+            int bekér = int.Parse(Console.ReadLine());
+            int szamlalo = 0;
+            for (int i = 1; i == bekér; i++)
+            {
+                if (bekér % i ==0)
+                {
+                    szamlalo++;
+                }
+                
+            }
+            if (szamlalo == 2)
+            {
+                Console.WriteLine("Az általad beírt szám PRÍM szám!");
+            }
+            else
+            {
+                Console.WriteLine("Az általad beírt szám NEM PRÍM szám!");
+            }
+        }
+    }
+}
