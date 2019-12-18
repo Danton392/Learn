8a6264b7a25ecf4c018a34e281f0e8589734ce11
Csatlós Endre <danton392@gmail.com>
2019-12-18 18:14:27 +0100

    Tömb Legkisebb/nagyobb eleme kiírása

---
+++ 20191218Lesson6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tömb = new int[15];
+            Random szam = new Random(); 
+            for (int i = 0; i <tömb.Length; i++)
+            {
+                tömb[i] = szam.Next(0,101);
+            }
+            Console.Write("Tömb elemei:");
+            for (int i = 0; i < tömb.Length; i++)
+            {
+                Console.Write(" "+tömb[i]);
+            }
+            Console.WriteLine();
+            int max = 0;
+            for (int i = 1; i < tömb.Length; i++)
+            {
+                if (tömb[max]<tömb[i])
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine("Tömb legnagyobb eleme: " + tömb[max] + " ,indexe: " + max);
+
+            int min = 0;
+            for (int i = 1; i < tömb.Length; i++)
+            {
+                if (tömb[min] > tömb[i])
+                {
+                    min = i;
+                }
+            }
+            Console.WriteLine("Tömb legkisebb eleme: " + tömb[min] + " ,indexe: " + min);
+
+        }
+    }
+}
